7db590914006e353b87106e066a5797ac57016d4
Ruslan <ruslanworkconsult@gmail.com>
2019-11-25 15:20:38 +0200

    All work (before eslint install)

---
+++ src/helpers/common.helper.js
@@ -0,0 +1,3 @@
+export function fff() {
+    console.log('fff');
+}
---
+++ src/js/common.js
@@ -0,0 +1,10 @@
+// import '../helpers/common.helper.js'
+import {fff} from "../helpers/common.helper"
+
+console.log('done');
+
+((a, b) => {
+    console.log(a + b);
+})(5, 3);
+
+fff();

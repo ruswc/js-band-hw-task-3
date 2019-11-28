c99b187a5d438cc120ed297680fe3ab2a7dfcbe0
Ruslan <ruslanworkconsult@gmail.com>
2019-11-28 11:33:30 +0200

    Creating transport implemented using factory pattern?? Creating Forms implemented using factory pattern

--- config/webpack.base.conf.js
+++ config/webpack.base.conf.js
@@ -20,7 +20,7 @@ module.exports = {
     output: {
         filename: `${PATHS.assets}js/[name].js`,
         path: PATHS.dist,
-        publicPath: "/"
+        publicPath: ""
     },
     module: {
         rules: [
